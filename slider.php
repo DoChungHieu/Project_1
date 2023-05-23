bbf77d4a81c4441efa5d17aa07e85d646fbe99a9
Trung Hiếu <dotrunghieu576@gmail.com>
2023-05-23 18:06:20 +0700

    firt commit

---
+++ slider.php
@@ -0,0 +1,14 @@
+    <!-- -----------------------SLlDER---------------------------------------------- -->
+    <section class="sliders">
+            <div class="aspect-ratio-169 sliders-img">
+                <img src="image/sl1.jpg" alt="">
+                <img src="image/sl2.jpg" alt="">
+                <img src="image/sl3.jpg" alt="">
+            </div>
+            <div class="dot-container">
+                <div class="dot active"></div>
+                <div class="dot"></div>
+                <div class="dot"></div>
+           </div>
+    </section>
+    <!-- -----------------------------------slider----------------------- -->
